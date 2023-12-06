b1a4f2e7115d0ce3f3dd47a0000c139035e4b422
Hamada-Gado <ahmedgado0100@gmail.com>
2023-12-06 19:00:18 +0200

    Feat: update StudentController

--- DatabaseProject/Controllers/StudentController.cs
+++ DatabaseProject/Controllers/StudentController.cs
@@ -90,5 +90,39 @@ namespace DatabaseProject.Controllers
                 }
             }
         }
+
+        public int get_student_id() { return 0; }
+
+        ///////////// PART 2 /////////////
+
+        // A
+        // View his/her graduation plan along with his/her assigned courses.
+        public ActionResult GraduationPlan()
+        {
+            int student_id = get_student_id();
+
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString);
+
+            using (con)
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.FN_StudentViewGP(@student_id)", con);
+                cmd.CommandType = CommandType.Text;
+
+                cmd.Parameters.Add("@student_id", SqlDbType.Int);
+                cmd.Parameters["@student_id"].Value = student_id;
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr);
+                }
+                rdr.Close();
+                con.Close();
+            }
+
+            return View();
+        }
+
     }
 }
