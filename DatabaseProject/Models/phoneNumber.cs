1d0f67911e92069881805df01f98bd4febf1d2b0
Mohamed Ahmed El Sawy <m.elsawy2003@gmail.com>
2023-12-15 22:21:40 +0200

    add phone number

---
+++ DatabaseProject/Models/phoneNumber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseProject.Models
+{
+    public class phoneNumber
+    {
+        public int Student_Phone { get; set; }
+        public int student_id { get; set; }
+    }
+}
