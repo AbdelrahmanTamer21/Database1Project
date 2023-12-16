b8140dffb8ccbc60f1137560aaa98eb71458a449
Abdelrahman Tamer <abdelrahmantamer2005@yahoo.com>
2023-12-16 19:01:48 +0200

    Admin studentTranscripts,GradPlanwithadvisors

---
+++ DatabaseProject/Models/Advisors_Graduation_Plan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseProject.Models
+{
+    public class Advisors_Graduation_Plan
+    {
+
+        public GraduationPlan graduationPlan { get; set; }
+        public Advisor advisor { get; set; }
+
+
+        public Advisors_Graduation_Plan() { }
+
+        public Advisors_Graduation_Plan(GraduationPlan graduationPlan, Advisor advisor)
+        {
+            this.graduationPlan = graduationPlan;
+            this.advisor = advisor;
+        }
+    }
+}
